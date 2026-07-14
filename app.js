822e0314679ca8fe93a33da9868a738a58ff85d8
SemaineFamille <melanie.weil.81@gmail.com>
2026-07-14 17:33:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
    Base + navigation + API + Présences + Menu
    Version optimisée
 ========================================================= */
-console.log("APP VERSION 14-07-2026 17h00");
+console.log("APP VERSION 14-07-2026 17h30");
 const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxOJEus1Fev5I4YZsSbjpIXXlgGJBY7QmFkkqZtkXYD6eEPwqmgCl8r2hfrO1X9eyqxSA/exec';
 const ADMIN_CODE = '2019';
 
