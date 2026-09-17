94401787b1a3296e9ba9cb31e8036710dce3d507
SemaineFamille <melanie.weil.81@gmail.com>
2026-09-17 14:54:14 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,19 @@
 console.log("api.js chargé");
+
+const APP_CACHE = {
+  responses: new Map(),
+  inflight: new Map(),
+  pagesLoaded: {
+    present: false,
+    menu: false,
+    courses: false,
+    taches: false,
+    job: false,
+    admin: false
+  },
+  presencesLoadedAt: 0,
+  presencesTtlMs: 60 * 1000,
+  menuLoadedAt: 0,
+  menuTtlMs: 60 * 1000,
+  badgesPromise: null
+};
