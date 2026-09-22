2e7bdac0c413af302a740d5872cde19e5c6b1b2e
SemaineFamille <melanie.weil.81@gmail.com>
2026-09-22 11:22:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log("APP VERSION 22-09-2026 11h15");
+console.log("APP VERSION 22-09-2026 11h20");
 
 /* =========================================================
    Cache front / anti-requêtes doublées
